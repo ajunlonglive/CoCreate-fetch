b936e8dd904330108dc8d3cf7bcd72b0c7cea8d1
frank pagan <frank@cocreate.app>
2021-08-13 23:55:18 +0000

    fix: remove logic component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import observer from '@cocreate/observer'
 import ccfilter from '@cocreate/filter'
 import utils from '@cocreate/utils';
 import crud from '@cocreate/crud-client';
-import logic from '@cocreate/logic';
+// import logic from '@cocreate/logic';
 import render from '@cocreate/render';
 
 const CoCreateFetch = {
